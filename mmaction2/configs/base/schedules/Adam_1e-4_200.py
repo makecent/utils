23d9d5a8646c432bc2615dca77e123f212ad46e3
louis-4 <luchongkai96@gmail.com>
2022-05-11 17:15:29 +0800

    add

--- mmaction2/configs/base/schedules/Adam_1e-4_200.py
+++
@@ -1,7 +0,0 @@
-# optimizer
-optimizer = dict(type='Adam', lr=1e-04)
-optimizer_config = dict(grad_clip=None)
-# learning policy
-lr_config = dict(policy='fixed')
-total_epochs = 200
-
---
+++ mmaction2/models/__init__.py
@@ -0,0 +1,5 @@
+from .resnet3d_sony import ResNet3d_sony
+from .mvit import MViTB
+__all__ = [
+    'ResNet3d_sony', 'MViTB'
+]
