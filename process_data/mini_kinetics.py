e105fccbafba4b5edf7f9967b28891cfa7cf539f
louis-4 <luchongkai96@gmail.com>
2022-07-03 19:38:30 +0800

    ff

--- process_data/mini_kinetics.py
+++ process_data/mini_kinetics.py
@@ -8,15 +8,17 @@ _, most_200_classes = zip(*sorted(zip(count, classes))[200:])
 
 train_sampled_list = []
 val_sampled_list = []
-for category in most_200_classes:
-    train_cate_list = train_list[train_list[1] == category]
+for new_class_id, class_id in enumerate(most_200_classes):
+    train_cate_list = train_list[train_list[1] == class_id]
+    train_cate_list[1] = new_class_id
     train_sampled_list.append(train_cate_list.sample(400))
 
-    val_cate_list = val_list[val_list[1] == category]
+    val_cate_list = val_list[val_list[1] == class_id]
+    val_cate_list[1] = new_class_id
     val_sampled_list.append(val_cate_list.sample(25))
 
 final_train_list = pd.concat(train_sampled_list).sample(frac=1)
 final_val_list = pd.concat(val_sampled_list).sample(frac=1)
 
-final_train_list.to_csv("/home/louis/PycharmProjects/ProgPreTrain/my_data/kinetics400/mini_kinetics200_train_list_videos.txt", header=False, index=False)
-final_val_list.to_csv("/home/louis/PycharmProjects/ProgPreTrain/my_data/kinetics400/mini_kinetics200_val_list_videos.txt", header=False, index=False)
+final_train_list.to_csv("/home/louis/PycharmProjects/ProgPreTrain/my_data/kinetics400/mini_kinetics200_train_list_videos.txt", header=False, index=False, sep=' ')
+final_val_list.to_csv("/home/louis/PycharmProjects/ProgPreTrain/my_data/kinetics400/mini_kinetics200_val_list_videos.txt", header=False, index=False, sep=' ')
